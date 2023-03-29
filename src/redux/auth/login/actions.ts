fa7caa4e325224bb75fa9251f3d33e67570da215
olfaselmi <olfa.selmi@esprit.tn>
2023-03-29 01:45:24 +0100

    first commit

---
+++ src/redux/auth/login/actions.ts
@@ -0,0 +1,37 @@
+import { AuthLoginActionTypes } from "./types";
+
+// common success
+export const authLoginApiResponseSuccess = (actionType: string, data: any) => ({
+  type: AuthLoginActionTypes.API_RESPONSE_SUCCESS,
+  payload: { actionType, data },
+});
+
+// common error
+export const authLoginApiResponseError = (
+  actionType: string,
+  error: string
+) => ({
+  type: AuthLoginActionTypes.API_RESPONSE_ERROR,
+  payload: { actionType, error },
+});
+
+export const loginUser = (user: any) => {
+  console.log("olfo");
+  return {
+    type: AuthLoginActionTypes.LOGIN_USER,
+    payload: { user },
+  };
+};
+
+export const logoutUser = () => {
+  return {
+    type: AuthLoginActionTypes.LOGOUT_USER,
+  };
+};
+
+export const socialLogin = (data: any, type: "facebook" | "google") => {
+  return {
+    type: AuthLoginActionTypes.SOCIAL_LOGIN,
+    payload: { data, type },
+  };
+};
